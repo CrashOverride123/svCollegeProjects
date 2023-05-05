c5c7de1dc1fb404d1a62aac0647c058dad4e5e76
CrashOverride123 <DarkSpirit57@gmail.com>
2023-05-05 20:25:12 +0300

    updates

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,13 @@
 //============================================================================
 //
 //============================================================================
+iValid = () => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('psw').value;
+  if (email.indexOf(`@`) == -1 && password.length == '') {
+    alert('Please type your information');
+  } else {
+  }
+};
+window.addEventListener('inspection', iValid);
+document.getElementById('exec').addEventListener('click', iValid);
